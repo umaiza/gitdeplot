8f3ffd7f817e11b82d5afe8556cf1f69dc70dfa7
umair amjad <umair amjad>
2016-04-15 22:02:06 +0500

    a

--- github.php
+++ github.php
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-<?php // Prevent accidental XSS 
-header("Content-type: text/plain"); 
-// Run the script 
-if ( $_POST['payload'] ) { shell_exec("./pull.sh"); }
-=======
 <?php // Prevent accidental XSS
  header("Content-type: text/plain"); 
  // Run the script 
@@ -11,4 +5,3 @@ if ( $_POST['payload'] ) { shell_exec("./pull.sh"); }
 { 
 	shell_exec("./pull.sh"); 
 }
->>>>>>> 61b4c52424e9486543ec2d4670f91ceb117bb3c3
